fc8b66f5d22831132a94712bc323be770604082b
Giles Smart <3227792+gilessmart@users.noreply.github.com>
2025-05-26 21:43:20 +0100

    Change to static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/mis-verbos",
+  output: "export"
 };
 
 export default nextConfig;
