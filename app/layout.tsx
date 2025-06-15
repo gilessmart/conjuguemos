02f2969667c3fee37d1600cab4889a751a20e339
Giles Smart <3227792+gilessmart@users.noreply.github.com>
2025-06-15 13:57:50 +0100

    Install MUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { Roboto } from 'next/font/google';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto"
+});
 
 export const metadata: Metadata = {
   title: "¡Conjuguemos!",
@@ -12,9 +22,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.variable}>
       <body>
-        {children}
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
