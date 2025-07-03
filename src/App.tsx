41bf6969d204991a9dbdc7832e547f60dd725bc1
gilessmart <3227792+gilessmart@users.noreply.github.com>
2025-07-03 16:25:57 +0100

    Remove some unused assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import VerbList from "./pages/VerbList";
 import VerbDetail from "./pages/VerbDetail";
-import './App.css'
 
 function App() {
   return (
