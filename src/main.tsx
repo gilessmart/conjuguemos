b54d3b719396fd5131c2fbd5844891aea09353e4
gilessmart <3227792+gilessmart@users.noreply.github.com>
2025-07-13 18:40:35 +0100

    Expand linting

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx'
 
-const base = import.meta.env.VITE_BASE_PATH;
+const base = typeof import.meta.env.VITE_BASE_PATH == "string"
+  ? import.meta.env.VITE_BASE_PATH
+  : undefined;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
