bd2aa0e74e26c4976ff2189d3374368d233c516f
gilessmart <3227792+gilessmart@users.noreply.github.com>
2025-08-16 16:23:45 +0100

    Use zod to parse settings

--- src/data/settings.hydration.ts
+++ src/data/settings.hydration.ts
@@ -1,64 +1,40 @@
+import zod from "zod";
 import type { Settings } from "./settings";
+
+const StoredSettings = zod.object({
+  moodInclusion: zod.array(zod.object({
+    moodName: zod.string(),
+    included: zod.boolean().default(true),
+    tenseInclusion: zod.array(zod.object({
+      tenseName: zod.string(),
+      included: zod.boolean().default(true)
+    })).default([])
+  })).default([]),
+  includeVosotros: zod.boolean().default(true)
+});
 
 export function hydrateSettings(settings: Settings, json: string): void {
   let storedSettingsObj: unknown;
   try { storedSettingsObj = JSON.parse(json); }
   catch { return; }
 
-  if (typeof storedSettingsObj !== "object" || storedSettingsObj === null || Array.isArray(storedSettingsObj))
+  const storedSettingsParseResult = StoredSettings.safeParse(storedSettingsObj);
+  if (!storedSettingsParseResult.success)
     return;
-  
-  const storedMoodInclusionsArr = "moodInclusion" in storedSettingsObj && typeof storedSettingsObj.moodInclusion === "object" && Array.isArray(storedSettingsObj.moodInclusion) 
-    ? storedSettingsObj.moodInclusion as unknown[]
-    : undefined;
 
-  if (storedMoodInclusionsArr !== undefined) {
-    for (const storedMoodInclusionsItem of storedMoodInclusionsArr) {
-      const storedMoodInclusionsItemObj = typeof storedMoodInclusionsItem === "object" && storedMoodInclusionsItem != null ? storedMoodInclusionsItem : undefined;
-      if (storedMoodInclusionsItemObj === undefined)
+  const storedSettings = storedSettingsParseResult.data;
+
+  for (const moodInclusion of settings.moodInclusion) {
+    const storedMoodInclusion = storedSettings.moodInclusion.find(mi => mi.moodName === moodInclusion.moodName);
+    if (storedMoodInclusion === undefined)
+      continue;
+    moodInclusion.included = storedMoodInclusion.included;
+    for (const tenseInclusion of moodInclusion.tenseInclusion) {
+      const storedTenseInclusion = storedMoodInclusion.tenseInclusion.find(ti => ti.tenseName === tenseInclusion.tenseName);
+      if (storedTenseInclusion === undefined)
         continue;
-      
-      if (!("moodName" in storedMoodInclusionsItemObj && typeof storedMoodInclusionsItemObj.moodName === "string"))
-        continue;
-      
-      const resultMoodInclusionItem = settings.moodInclusion.find(x => x.moodName === storedMoodInclusionsItemObj.moodName);
-      if (resultMoodInclusionItem === undefined)
-        continue;
-
-      if (!("included" in storedMoodInclusionsItemObj && typeof storedMoodInclusionsItemObj.included === "boolean"))
-        continue;
-
-      resultMoodInclusionItem.included = storedMoodInclusionsItemObj.included;
-      
-      if (!("tenseInclusion" in storedMoodInclusionsItemObj && typeof storedMoodInclusionsItemObj.tenseInclusion === "object"))
-        continue;
-
-      const tenseInclusionArr = Array.isArray(storedMoodInclusionsItemObj.tenseInclusion) ? storedMoodInclusionsItemObj.tenseInclusion as unknown[] : undefined;
-      if (tenseInclusionArr === undefined)
-        continue;
-
-      for (const tenseInclusionItem of tenseInclusionArr) {
-        const tenseInclusionItemObj = typeof tenseInclusionItem === "object" && tenseInclusionItem !== null ? tenseInclusionItem : undefined;
-        if (tenseInclusionItemObj === undefined)
-          continue;
-
-        if (!("tenseName" in tenseInclusionItemObj && typeof tenseInclusionItemObj.tenseName === "string"))
-          continue;
-      
-        const resultTenseInclusionItem = resultMoodInclusionItem.tenseInclusion.find(x => x.tenseName === tenseInclusionItemObj.tenseName);
-        if (resultTenseInclusionItem === undefined)
-          continue;
-
-        if (!("included" in tenseInclusionItemObj && typeof tenseInclusionItemObj.included === "boolean"))
-          continue;
-      
-        resultTenseInclusionItem.included = tenseInclusionItemObj.included;
-      }
+      tenseInclusion.included = storedTenseInclusion.included;
     }
   }
-
-  if ("includeVosotros" in storedSettingsObj && typeof storedSettingsObj.includeVosotros === "boolean")
-    settings.includeVosotros = storedSettingsObj.includeVosotros;
-
-  return;
+  settings.includeVosotros = storedSettings.includeVosotros;    
 }
