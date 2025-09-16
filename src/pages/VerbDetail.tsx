a91d8ac1eb2aa59672568b0d2349b2b7a1ae019d
gilessmart <3227792+gilessmart@users.noreply.github.com>
2025-09-16 23:41:18 +0100

    Allow verb table to scroll left & right

--- src/pages/VerbDetail.tsx
+++ src/pages/VerbDetail.tsx
@@ -38,101 +38,105 @@ export default function VerbDetail() {
 
         <h3>Indicative</h3>
 
-        <table className={styles.verbTable}>
-          <thead>
-            <tr>
-              <th>Pronoun(s)</th>
-              <th>Present</th>
-              <th>Preterite</th>
-              <th>Imperfect</th>
-              <th>Future</th>
-              <th>Conditional</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>yo</td>
-              <td>{moods.indicative.present.firstSingular.value}</td>
-              <td>{moods.indicative.preterite.firstSingular.value}</td>
-              <td>{moods.indicative.imperfect.firstSingular.value}</td>
-              <td>{moods.indicative.future.firstSingular.value}</td>
-              <td>{moods.indicative.conditional.firstSingular.value}</td>
-            </tr>
-            <tr>
-              <td>tú</td>
-              <td>{moods.indicative.present.secondSingularInformal.value}</td>
-              <td>{moods.indicative.preterite.secondSingularInformal.value}</td>
-              <td>{moods.indicative.imperfect.secondSingularInformal.value}</td>
-              <td>{moods.indicative.future.secondSingularInformal.value}</td>
-              <td>{moods.indicative.conditional.secondSingularInformal.value}</td>
-            </tr>
-            <tr>
-              <td>él / ella / usted</td>
-              <td>{moods.indicative.present.thirdSingularAndSecondSingularFormal.value}</td>
-              <td>{moods.indicative.preterite.thirdSingularAndSecondSingularFormal.value}</td>
-              <td>{moods.indicative.imperfect.thirdSingularAndSecondSingularFormal.value}</td>
-              <td>{moods.indicative.future.thirdSingularAndSecondSingularFormal.value}</td>
-              <td>{moods.indicative.conditional.thirdSingularAndSecondSingularFormal.value}</td>
-            </tr>
-            <tr>
-              <td>nosotros / nosotras</td>
-              <td>{moods.indicative.present.firstPlural.value}</td>
-              <td>{moods.indicative.preterite.firstPlural.value}</td>
-              <td>{moods.indicative.imperfect.firstPlural.value}</td>
-              <td>{moods.indicative.future.firstPlural.value}</td>
-              <td>{moods.indicative.conditional.firstPlural.value}</td>
-            </tr>
-            <tr>
-              <td>vosotros / vosotras</td>
-              <td>{moods.indicative.present.secondPluralInformal.value}</td>
-              <td>{moods.indicative.preterite.secondPluralInformal.value}</td>
-              <td>{moods.indicative.imperfect.secondPluralInformal.value}</td>
-              <td>{moods.indicative.future.secondPluralInformal.value}</td>
-              <td>{moods.indicative.conditional.secondPluralInformal.value}</td>
-            </tr>
-            <tr>
-              <td>ellos / ellas / ustedes</td>
-              <td>{moods.indicative.present.thirdPluralAndSecondPluralFormal.value}</td>
-              <td>{moods.indicative.preterite.thirdPluralAndSecondPluralFormal.value}</td>
-              <td>{moods.indicative.imperfect.thirdPluralAndSecondPluralFormal.value}</td>
-              <td>{moods.indicative.future.thirdPluralAndSecondPluralFormal.value}</td>
-              <td>{moods.indicative.conditional.thirdPluralAndSecondPluralFormal.value}</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className={styles.verbTableWrapper}>
+          <table className={styles.verbTable}>
+            <thead>
+              <tr>
+                <th>Pronoun(s)</th>
+                <th>Present</th>
+                <th>Preterite</th>
+                <th>Imperfect</th>
+                <th>Future</th>
+                <th>Conditional</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>yo</td>
+                <td>{moods.indicative.present.firstSingular.value}</td>
+                <td>{moods.indicative.preterite.firstSingular.value}</td>
+                <td>{moods.indicative.imperfect.firstSingular.value}</td>
+                <td>{moods.indicative.future.firstSingular.value}</td>
+                <td>{moods.indicative.conditional.firstSingular.value}</td>
+              </tr>
+              <tr>
+                <td>tú</td>
+                <td>{moods.indicative.present.secondSingularInformal.value}</td>
+                <td>{moods.indicative.preterite.secondSingularInformal.value}</td>
+                <td>{moods.indicative.imperfect.secondSingularInformal.value}</td>
+                <td>{moods.indicative.future.secondSingularInformal.value}</td>
+                <td>{moods.indicative.conditional.secondSingularInformal.value}</td>
+              </tr>
+              <tr>
+                <td>él/ella/usted</td>
+                <td>{moods.indicative.present.thirdSingularAndSecondSingularFormal.value}</td>
+                <td>{moods.indicative.preterite.thirdSingularAndSecondSingularFormal.value}</td>
+                <td>{moods.indicative.imperfect.thirdSingularAndSecondSingularFormal.value}</td>
+                <td>{moods.indicative.future.thirdSingularAndSecondSingularFormal.value}</td>
+                <td>{moods.indicative.conditional.thirdSingularAndSecondSingularFormal.value}</td>
+              </tr>
+              <tr>
+                <td>nosotros/nosotras</td>
+                <td>{moods.indicative.present.firstPlural.value}</td>
+                <td>{moods.indicative.preterite.firstPlural.value}</td>
+                <td>{moods.indicative.imperfect.firstPlural.value}</td>
+                <td>{moods.indicative.future.firstPlural.value}</td>
+                <td>{moods.indicative.conditional.firstPlural.value}</td>
+              </tr>
+              <tr>
+                <td>vosotros/vosotras</td>
+                <td>{moods.indicative.present.secondPluralInformal.value}</td>
+                <td>{moods.indicative.preterite.secondPluralInformal.value}</td>
+                <td>{moods.indicative.imperfect.secondPluralInformal.value}</td>
+                <td>{moods.indicative.future.secondPluralInformal.value}</td>
+                <td>{moods.indicative.conditional.secondPluralInformal.value}</td>
+              </tr>
+              <tr>
+                <td>ellos/ellas/ustedes</td>
+                <td>{moods.indicative.present.thirdPluralAndSecondPluralFormal.value}</td>
+                <td>{moods.indicative.preterite.thirdPluralAndSecondPluralFormal.value}</td>
+                <td>{moods.indicative.imperfect.thirdPluralAndSecondPluralFormal.value}</td>
+                <td>{moods.indicative.future.thirdPluralAndSecondPluralFormal.value}</td>
+                <td>{moods.indicative.conditional.thirdPluralAndSecondPluralFormal.value}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
 
         <h3>Imperative</h3>
 
-        <table className={styles.verbTable}>
-          <thead>
-            <tr>
-              <th>Pronoun(s)</th>
-              <th>Affirmative</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>tú</td>
-              <td>{moods.imperative.affirmative.secondSingularInformal.value}</td>
-            </tr>
-            <tr>
-              <td>usted</td>
-              <td>{moods.imperative.affirmative.secondSingularFormal.value}</td>
-            </tr>
-            <tr>
-              <td>nosotros / nosotras</td>
-              <td>{moods.imperative.affirmative.firstPlural.value}</td>
-            </tr>
-            <tr>
-              <td>vosotros / vosotras</td>
-              <td>{moods.imperative.affirmative.secondPluralInformal.value}</td>
-            </tr>
-            <tr>
-              <td>ustedes</td>
-              <td>{moods.imperative.affirmative.secondPluralFormal.value}</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className={styles.verbTableWrapper}>
+          <table className={styles.verbTable}>
+            <thead>
+              <tr>
+                <th>Pronoun(s)</th>
+                <th>Affirmative</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>tú</td>
+                <td>{moods.imperative.affirmative.secondSingularInformal.value}</td>
+              </tr>
+              <tr>
+                <td>usted</td>
+                <td>{moods.imperative.affirmative.secondSingularFormal.value}</td>
+              </tr>
+              <tr>
+                <td>nosotros/nosotras</td>
+                <td>{moods.imperative.affirmative.firstPlural.value}</td>
+              </tr>
+              <tr>
+                <td>vosotros/vosotras</td>
+                <td>{moods.imperative.affirmative.secondPluralInformal.value}</td>
+              </tr>
+              <tr>
+                <td>ustedes</td>
+                <td>{moods.imperative.affirmative.secondPluralFormal.value}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </main>
     </div>
   );
