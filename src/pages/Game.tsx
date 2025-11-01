7b8fdcddf86670a0374f20a876d241a1779cdf28
gilessmart <3227792+gilessmart@users.noreply.github.com>
2025-11-01 19:06:55 +0000

    Remove "Show answer" button; force user to guess & tell them if they were wrong

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useId, useRef, useState } from "react";
+import { useId, useState, type ReactNode } from "react";
 import { getRandomVerb } from "../data/verbs";
 import { usePageTitle } from "../hooks/usePageTitle";
 import { chooseRandomElement } from "../utils/chooseRandom";
@@ -7,58 +7,71 @@ import { getSettings, isPersonEnabled, isTenseEnabled } from "../data/settings";
 import type { Conjugation, Mood, Tense } from "../data/verbs.types";
 import Header from "../components/Header";
 
+type GameState =  "EnteringAnswer" | "AnswerCorrect" | "AnswerIncorrect";
+
 export default function Game() {
   usePageTitle("");
   const inputId = useId();
   const [answer, setAnswer] = useState("");
   const [target, setTarget] = useState(generateTarget());
-  const [showAnswer, setShowAnswer] = useState(false);
-  const [submissionIncorrect, setSubmissionIncorrect] = useState(false);
-  const answerTimeout = useRef<NodeJS.Timeout | null>(null);
+  const [gamesState, setGameState] = useState<GameState>("EnteringAnswer");
 
-  useEffect(() => {
-    return () => {
-      if (answerTimeout.current) {
-        clearTimeout(answerTimeout.current);
-      }
-    };
-  }, []);
-
-  function updateAnswer(newAnswer: string) {
-    setSubmissionIncorrect(false);
+  function answerUpdated(newAnswer: string) {
     setAnswer(newAnswer);
-    endShowAnswer();
   }
 
-  function submitAnswer() {
-    if (answer.trim().length === 0)
-      return;
+  function submitClicked() {
+    switch (gamesState) {
+      case "EnteringAnswer":
+        if (answerEmpty()) return;
+        if (isAnswerCorrect()) setGameState("AnswerCorrect");
+        else setGameState("AnswerIncorrect");
+        break;
 
-    if (isAnswerCorrect()) {
-      setTarget(generateTarget());
-      setAnswer("");
+      case "AnswerCorrect":
+      case "AnswerIncorrect":
+        setTarget(generateTarget());
+        setAnswer("");
+        setGameState("EnteringAnswer");
+        break;
     }
-    else {
-      setSubmissionIncorrect(true);
-    }
+  }
+
+  function answerEmpty() {
+    return answer.trim().length === 0;
   }
 
   function isAnswerCorrect(): boolean {
     return answer.trim().toLowerCase() === target.conjugation.value;
   }
 
-  function startShowAnswer() {
-    setShowAnswer(true);
-    answerTimeout.current = setTimeout(() => {
-      endShowAnswer();
-    }, 2500);
+  function renderInstructions(): ReactNode {
+    switch (gamesState) {
+      case "EnteringAnswer":
+        return <div className={styles.instructions}>
+          <div>Conjugate the verb!</div>
+        </div>;
+      case "AnswerCorrect":
+        return <div className={`${styles.instructions} ${styles.correct}`}>
+          <div>Correct!</div>
+        </div>;
+      case "AnswerIncorrect":
+        return <div className={`${styles.instructions} ${styles.incorrect}`}>
+          <div>
+            Incorrect<br />
+            Expected <span className={styles.expected}>{target.conjugation.value}</span>
+          </div>
+        </div>;
+    }
   }
 
-  function endShowAnswer() {
-    setShowAnswer(false);
-    if (answerTimeout.current) {
-      clearTimeout(answerTimeout.current);
-      answerTimeout.current = null;
+  function getSubmitButtonLabel(): ReactNode {
+    switch (gamesState) {
+      case "EnteringAnswer":
+        return "Check";
+      case "AnswerCorrect":
+      case "AnswerIncorrect":
+        return "Next";
     }
   }
 
@@ -66,37 +79,37 @@ export default function Game() {
     <>
       <Header title="Conjuguemos" />
       <main>
-        <form>
-          <div className={styles.targetDetails}>
-            <span className={styles.label}>verb</span><span className={styles.value}>{target.verb.infinitive}</span>
-            <span className={styles.label}>tense</span><span className={styles.value}>{target.mood.description} {target.tense.description}</span>
-          </div>
-          
+        {renderInstructions()}
+
+        <div className={styles.targetDetails}>
+          <span className={styles.label}>verb</span><span className={styles.value}>{target.verb.infinitive}</span>
+          <span className={styles.label}>tense</span><span className={styles.value}>{target.mood.description} {target.tense.description}</span>
+        </div>
+
+        <form>          
           <div className={styles.verbInputRow}>
             <label htmlFor={inputId}>{target.pronoun}</label>
             <div>
               <input type="text" id={inputId} value={answer}
                      autoCapitalize="off" autoComplete="off"
-                     onChange={e => { updateAnswer(e.target.value); }}
-                     className={submissionIncorrect ? styles.submissionIncorrect : ''}/>
+                     disabled={gamesState !== "EnteringAnswer"}
+                     onChange={e => { answerUpdated(e.target.value); }} />
               <div className={styles.accentChars}>
-                <button type="button" onClick={ () => { updateAnswer(answer + "á"); } }>á</button>
-                <button type="button" onClick={ () => { updateAnswer(answer + "é"); } }>é</button>
-                <button type="button" onClick={ () => { updateAnswer(answer + "í"); } }>í</button>
-                <button type="button" onClick={ () => { updateAnswer(answer + "ñ"); } }>ñ</button>
-                <button type="button" onClick={ () => { updateAnswer(answer + "ó"); } }>ó</button>
-                <button type="button" onClick={ () => { updateAnswer(answer + "ú"); } }>ú</button>
+                <button type="button" disabled={gamesState !== "EnteringAnswer"} onClick={ () => { answerUpdated(answer + "á"); } }>á</button>
+                <button type="button" disabled={gamesState !== "EnteringAnswer"} onClick={ () => { answerUpdated(answer + "é"); } }>é</button>
+                <button type="button" disabled={gamesState !== "EnteringAnswer"} onClick={ () => { answerUpdated(answer + "í"); } }>í</button>
+                <button type="button" disabled={gamesState !== "EnteringAnswer"} onClick={ () => { answerUpdated(answer + "ñ"); } }>ñ</button>
+                <button type="button" disabled={gamesState !== "EnteringAnswer"} onClick={ () => { answerUpdated(answer + "ó"); } }>ó</button>
+                <button type="button" disabled={gamesState !== "EnteringAnswer"} onClick={ () => { answerUpdated(answer + "ú"); } }>ú</button>
               </div>
             </div>
           </div>
 
-          <div className={styles.buttonRow}>
-            <div className={styles.showAnswerWrapper}>
-              { !showAnswer && <button className={styles.showAnswerButton} type="button" onClick={() => { startShowAnswer(); }}>Show answer</button> }
-              { showAnswer && <span className={styles.answer}>{target.conjugation.value}</span> }
-            </div>
-            <button type="submit" className={styles.submitButton} onClick={e => { e.preventDefault(); submitAnswer(); }}>Check</button>
-          </div>
+          <button type="submit" className={styles.submitButton} 
+                  disabled={answerEmpty()}
+                  onClick={e => { e.preventDefault(); submitClicked(); }}>
+            {getSubmitButtonLabel()}
+          </button>
         </form>
       </main>
     </>
